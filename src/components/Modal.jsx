b1fadb214bfe1fd5721ed73147a6a8ca0805133f
gyubo <yoon9yubo@gmail.com>
2025-01-13 22:27:54 +0900

    dependency list

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,19 +1,19 @@
-import { forwardRef, useImperativeHandle, useRef } from "react";
+import { useRef, useEffect } from "react";
 import { createPortal } from "react-dom";
 
-const Modal = forwardRef(function Modal({ children }, ref) {
+function Modal({ open, children }) {
   const dialog = useRef();
 
-  useImperativeHandle(ref, () => {
-    return {
-      open: () => {
-        dialog.current.showModal();
-      },
-      close: () => {
-        dialog.current.close();
-      },
-    };
-  });
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal(); // 모달로 열기
+    } else {
+      dialog.current.close();
+    }
+  }, [open]);
+
+  // effect dependency란 속성이나 상태값으로 이해 할 수 있다.
+  // 컴포넌트 함수를 다시 실행하도록 하는 값 -> 해당 함수가 실행될때마다 작동해야하기 때문
 
   return createPortal(
     <dialog className="modal" ref={dialog}>
@@ -21,6 +21,6 @@ const Modal = forwardRef(function Modal({ children }, ref) {
     </dialog>,
     document.getElementById("modal")
   );
-});
+}
 
 export default Modal;
